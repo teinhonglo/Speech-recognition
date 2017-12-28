85c3d9b1b3a52d25cba95a758023e46ba9e17a04
teinhonglo <teinhonglo@gmail.com>
2017-12-28 21:36:13 +0800

    timit script

---
+++ Kaldi-script/TIMIT/s5/cmd.sh
@@ -0,0 +1,33 @@
+# "queue.pl" uses qsub.  The options to it are
+# options to qsub.  If you have GridEngine installed,
+# change this to a queue you have access to.
+# Otherwise, use "run.pl", which will run jobs locally
+# (make sure your --num-jobs options are no more than
+# the number of cpus on your machine.
+
+#a) JHU cluster options
+#export train_cmd="queue.pl -l arch=*64"
+#export decode_cmd="queue.pl -l arch=*64,mem_free=2G,ram_free=2G"
+#export mkgraph_cmd="queue.pl -l arch=*64,ram_free=4G,mem_free=4G"
+#export big_memory_cmd="queue.pl -l arch=*64,ram_free=8G,mem_free=8G"
+#export cuda_cmd="queue.pl -l gpu=1"
+
+
+
+#b) BUT cluster options
+#export train_cmd="queue.pl -q all.q@@blade -l ram_free=1200M,mem_free=1200M"
+#export decode_cmd="queue.pl -q all.q@@blade -l ram_free=1700M,mem_free=1700M"
+#export decodebig_cmd="queue.pl -q all.q@@blade -l ram_free=4G,mem_free=4G"
+
+#export cuda_cmd="queue.pl -q long.q@@pco203 -l gpu=1"
+#export cuda_cmd="queue.pl -q long.q@pcspeech-gpu"
+#export mkgraph_cmd="queue.pl -q all.q@@servers -l ram_free=4G,mem_free=4G"
+
+#c) run it locally...
+#export train_cmd="queue.pl -q all.q@nv550"
+export train_cmd="queue.pl --config conf/queue_mod.conf"
+export decode_cmd="queue.pl --config conf/queue_mod.conf"
+export mkgraph_cmd="queue.pl --config conf/queue_mod.conf"
+export big_memory_cmd="queue.pl --config conf/queue_mod.conf"
+export cuda_cmd="queue.pl -q all.q@titan"    #會完全佔用一張顯卡
+export cuda_light_cmd="queue.pl -q all.q@titan"    #只會要求記憶體容量,不會完全佔用GPU
